5f1e1135bebedd6407663ec7274796217bcfdb77
Paul-Sebas-Pro <paul.sebas@free.fr>
2026-02-28 21:37:21 +0100

    Création du formulaire

--- todo-list-react/src/App.tsx
+++ todo-list-react/src/App.tsx
@@ -1,8 +1,32 @@
+interface IPriority {
+  level: "Urgente" | "Moyenne" | "Basse";
+}
+
+interface ITodo {
+  id: number;
+  text: string;
+  priority: IPriority;
+}
+
 function App() {
   return (
     <>
-      <div className="bg-red-400">
-        <button className="btn btn-accent">test</button>
+      <div className="flex justify-center">
+        <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
+          <div className="flex gap-4">
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="Ajouter une tâche..."
+            />
+            <select className="select w-full">
+              <option value="Urgente">Urgente</option>
+              <option value="Moyenne">Moyenne</option>
+              <option value="Basse">Basse</option>
+            </select>
+            <button className="btn btn-primary">Ajouter</button>
+          </div>
+        </div>
       </div>
     </>
   );
